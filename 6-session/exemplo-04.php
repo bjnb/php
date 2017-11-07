3aea5db82b1cc0ce790b46683993f5ae71cc8790
Bruno Jorge <bjnb@cin.ufpe.br>
2017-11-06 21:26:31 -0300

    Update: regenerate id modificado

--- 6-session/exemplo-04.php
+++ 6-session/exemplo-04.php
@@ -1,13 +1,13 @@
 <?php 
 
 	// Recuperando o ID
-	session_id('hkdo2ifblccvha3dtdoigvnou5');
+	//echo session_id('di54cb56c24ol1v1unnoola1tg');
 
 	require_once("session.php");
 
-	//session_regenerate_id();
+	session_regenerate_id( TRUE );
 
-	echo session_id('tmkko642m1h4gc1qat9af884ca');
+	echo session_id().'<br>';
 
 	var_dump($_SESSION);
 
@@ -15,6 +15,12 @@
 		di54cb56c24ol1v1unnoola1tg
 		hkdo2ifblccvha3dtdoigvnou5
 		tmkko642m1h4gc1qat9af884ca
+
+
+		b2pl1qlqc6kriedmg4m8t4306s
+		rb1ciqdbufu7mqvp7mubhla11f
+		m3hep22togsh8ha3nnbn9vftb7
+		rp0jak7br0vf2u6137dthmbdtu
 	*/
 
  ?>
