9a25c71d15bb38e9b3399d93457eb470b37df16b
Bruno Jorge <bjnb@cin.ufpe.br>
2017-11-07 22:47:10 -0300

    Métodos Mágicos finalizado

--- 9-OO/exemplo-04.php
+++ 9-OO/exemplo-04.php
@@ -1,9 +1,46 @@
 <?php
 
 	/*
-		Declaração de classe
+		Métodos Mágicos
 	*/
 
-	
+	class Endereco {
+		
+		private $logradouro;
+		private $numero;
+		private $cidade;
+
+		public function __construct( $a, $b, $c ) {
+			
+			$this->logradouro = $a;
+			$this->numero = $b;
+			$this->cidade = $c;
+			
+		}
+
+		public function __destruct() {
+
+			var_dump("Destruct chamado!");
+
+		}
+
+		public function __toString() {
+			return 	$this->logradouro . ", ".
+					$this->numero . " - " .
+					$this->cidade;
+		}
+
+	}
+
+
+	$e1 = new Endereco("Avenida Doutor José Rufino", "514", "Recife");
+
+	var_dump($e1);
+
+	echo '<br><br>';
+
+	echo $e1.'<br><br>';
+
+	unset($e1);
 
 ?>
