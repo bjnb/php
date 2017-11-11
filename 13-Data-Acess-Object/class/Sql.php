e06f368f3e36c54f8fcc04735a7e88576a304bee
Bruno Jorge <bjnb@cin.ufpe.br>
2017-11-11 04:32:57 -0300

    Update: Select funcionando

---
+++ 13-Data-Acess-Object/class/Sql.php
@@ -0,0 +1,76 @@
+<?php 
+
+	interface BD {
+
+		// Métodos abstratos
+		public function select( $rowQuery, $params ):array;
+
+	};
+
+	class Sql extends PDO implements BD {
+		
+		private $conn;
+		private $statement;
+
+		// ========================================================
+		// 	Funções públicas
+		
+		public function __construct() {
+
+			$this->conn = new PDO(
+				"mysql:dbname=dbphp7;host=localhost",
+				"root",
+				""
+			);
+
+		}
+
+		public function select( $rowQuery, $params = array() ):array {
+			$this->setQuery( $rowQuery, $params );
+
+			return $this->statement->fetchAll( PDO::FETCH_ASSOC );
+		}
+
+		/*
+		public function insert( $rowQuery, $params = array() ):int {
+
+		}
+		*/
+
+		// ========================================================
+
+
+		// ========================================================
+		//	Funções privadas para funcionamento interno
+
+		private function setQuery( $rowQuery, $params = array() ) {
+
+			$this->setStatment( $rowQuery );
+
+			$this->setParams( $params );
+
+			$this->statement->execute();
+		}
+
+		private function setParams( $parameters = array() ) {
+			foreach ( $parameters as $key => $value ) {
+				$this->setParam( $key, $value );
+			}
+		}
+
+		private function setParam( $key, $value ) {
+			$this->statement->bindParam( $key, $value );
+		}
+
+		private function setStatment( $rowQuery ) {
+			$this->statement = $this->conn->prepare( $rowQuery );
+		}
+
+		// ========================================================
+
+		// ========================================================
+		//	Funções mágicas
+
+	}
+
+?>
